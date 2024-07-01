3a0019630fd5d3204f0d41f460156f526641a11b
laihaoqiang <laihaoqiang34@gmail.com>
2024-07-02 00:53:13 +1000

    i18next usage understanding

---
+++ i18nConfig.ts
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+
+const i18nConfig = {
+    locales: ['en', 'fr', 'it'],
+    defaultLocale: 'en'
+  };
+  
+export default i18nConfig;
---
+++ src/app/[locale]/page.tsx
@@ -0,0 +1,11 @@
+// import styles from './page.module.css';
+import ExampleClientComponent from '@/components/ExampleClientComponent';
+
+export default async function Home() {
+  return (
+    <main>
+      <h1>Welcome to my home page</h1>
+      <ExampleClientComponent />
+    </main>
+  );
+}
